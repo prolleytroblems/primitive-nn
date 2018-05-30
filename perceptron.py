0273ac04410764d1af0b2d400f7a668219ccbea1
LucasN <oxiloxil@live.com>
2018-05-30 15:15:33 +0900

    Added classifier option

--- perceptron.py
+++ perceptron.py
@@ -115,41 +115,67 @@ class InputLayer(Layer):
         raise Exception()
 
 class Network:
-    def __init__(self, inputs, l_rate=0.1):
+    def __init__(self, inputs, l_rate=0.1, type="regressor"):
         assert type(inputs)==int
         self.inputs=inputs
         self.layers=[InputLayer(inputs)]
         self.l_rate=l_rate
+        self.__type=type
 
     def add_layer(self, nodes):
         self.layers.append(Layer(self.layers[len(self.layers)-1], nodes, l_rate=self.l_rate))
         return self
 
     def __call__(self, inputs):
-        inputs=np.array(inputs)
-        outputs=inputs
-        for layer in self.layers:
-            outputs=layer(outputs)
-        #print("final output", outputs)
-        return outputs
+        if self.__type=="regressor":
+            inputs=np.array(inputs)
+            outputs=inputs
+            for layer in self.layers:
+                outputs=layer(outputs)
+            #print("final output", outputs)
+            return outputs
+        elif self.__type=="classifier":
+            inputs=np.array(inputs)
+            outputs=inputs
+            for layer in self.layers:
+                outputs=layer(outputs)
+            exp_sum=sum([exp(output) for output in outputs])
+            outputs=[exp(output)/exp_sum for output in outputs]
+            return outputs
+        else:
+            raise Exception()
 
     def evaluate(self, dataset):
-        error=0
-        for key in dataset:
-            outputs=self(np.array(key))
-            for diff in dataset[key]-outputs:
-                error+=diff*diff
-        return error/len(dataset)
+        if self.__type=="regressor":
+            error=0
+            for key in dataset:
+                outputs=self(np.array(key))
+                for diff in dataset[key]-outputs:
+                    error+=diff*diff
+            return error/len(dataset)
+        elif self.__type=="classifier":
+            mistakes=0
+            for key in dataset:
+                outputs=self(np.array(key))
+                if np.where(outputs==max(outputs))!=np.where(np.array(dataset[key])==max(dataset[key])):
+                    mistakes+=1
+            return mistakes/len(dataset)
+        else:
+            raise Exception()
 
     def backprop(self, inputs, expectations):
-        outputs=self(inputs)
-        weighed_delta=np.array([(outputs-np.array(expectations))/len(outputs)])
-        #print("first delta", weighed_delta)
-        for i in range(1,len(self.layers)):
-            weighed_delta=np.array(self.layers[-i].backprop(weighed_delta))
-
-        #error= self.evaluate(inputs, expectations)
-        #return error
+        if type=="regressor":
+            outputs=self(inputs)
+            weighed_delta=np.array([(outputs-np.array(expectations))/len(outputs)])
+            for i in range(1,len(self.layers)):
+                weighed_delta=np.array(self.layers[-i].backprop(weighed_delta))
+        elif self.__type=="classifier":
+            outputs=self(inputs)
+            weighed_delta=np.array([(outputs-np.array(expectations))/len(outputs)])*outputs(1-outputs)
+            for i in range(1,len(self.layers)):
+                weighed_delta=np.array(self.layers[-i].backprop(weighed_delta))
+        else:
+            raise Exception()
 
     def train(self, data):
         assert isinstance(data, dict)
